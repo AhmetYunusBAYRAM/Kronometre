d1416e683b1a154d6e6e8837aa1cdd95792a11b0
Ahmet Yunus BAYRAM <84668188+AhmetYunusBAYRAM@users.noreply.github.com>
2021-10-24 21:19:16 +0300

    Add files via upload

---
+++ Kronometre/Kronometre/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Kronometre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbldk = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblsan = new System.Windows.Forms.Label();
+            this.lblsal = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbldk
+            // 
+            this.lbldk.AutoSize = true;
+            this.lbldk.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbldk.Location = new System.Drawing.Point(72, 9);
+            this.lbldk.Name = "lbldk";
+            this.lbldk.Size = new System.Drawing.Size(87, 63);
+            this.lbldk.TabIndex = 0;
+            this.lbldk.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(159, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 63);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ":";
+            // 
+            // lblsan
+            // 
+            this.lblsan.AutoSize = true;
+            this.lblsan.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsan.Location = new System.Drawing.Point(201, 9);
+            this.lblsan.Name = "lblsan";
+            this.lblsan.Size = new System.Drawing.Size(87, 63);
+            this.lblsan.TabIndex = 2;
+            this.lblsan.Text = "00";
+            // 
+            // lblsal
+            // 
+            this.lblsal.AutoSize = true;
+            this.lblsal.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsal.Location = new System.Drawing.Point(330, 9);
+            this.lblsal.Name = "lblsal";
+            this.lblsal.Size = new System.Drawing.Size(87, 63);
+            this.lblsal.TabIndex = 3;
+            this.lblsal.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(288, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 63);
+            this.label5.TabIndex = 4;
+            this.label5.Text = ":";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(212, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "DURDUR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblsal);
+            this.Controls.Add(this.lblsan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbldk);
+            this.Name = "Form1";
+            this.Text = "00";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbldk;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblsan;
+        private System.Windows.Forms.Label lblsal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Kronometre/Kronometre/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kronometre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int dak=0, san=0, sal=0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            button2.Text = "DURDUR";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sal++;
+            if (sal==100)
+            {
+                sal = 0;
+                san++;
+                if (san==60)
+                {
+                    san = 0;
+                    dak++;
+                }
+            }
+            lbldk.Text = String.Format("{0:D2}",dak);
+            lblsan.Text = String.Format("{0:D2}", san);
+            lblsal.Text = String.Format("{0:D2}", sal);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "DURDUR" && sal!=0)
+            {
+                button2.Text = "SIFIRLA";
+                timer1.Stop();
+            }
+            else
+            {
+                button2.Text = "DURDUR";
+                dak = 0;
+                san = 0;
+                sal = 0;
+                lbldk.Text = String.Format("{0:D2}", dak);
+                lblsan.Text = String.Format("{0:D2}", san);
+                lblsal.Text = String.Format("{0:D2}", sal);
+            }      
+        }
+    }
+}
